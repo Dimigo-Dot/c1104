06a8075d3fce309f64904ecc174f2b8c62781798
root <bboomin1320@dimigo.hs.kr>
2022-10-19 06:35:08 +0000

    c1104

---
+++ src/test.f.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void dimigoPrint(){
+	printf("한국디지털미디어고등학교");
+	printf("디미고\n");
+}
+
+int main(){
+	printf("나의 이름은 ~~~ 입니다.\n");
+	dimigoPrint();
+	printf("나의 성별은 ~~ 입니다.\n");
+	printf("한국디지털미디어고등학교\n");
+	dimigoPrint();
+	printf("한국디지털미디어고등학교");
+	dimigoPrint();
+		
+	return 0; }
